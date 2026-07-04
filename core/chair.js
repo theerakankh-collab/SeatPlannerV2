653928ddb0414a746730ea75fe708dfe083ca251
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 03:20:55 +0700

    Add Chair class with constructor and properties

--- core/chair.js
+++ core/chair.js
@@ -1,1 +1,17 @@
+export default class Chair{
 
+    constructor(id,x,y){
+
+        this.id=id;
+
+        this.x=x;
+
+        this.y=y;
+
+        this.person=null;
+
+        this.selected=false;
+
+    }
+
+}
