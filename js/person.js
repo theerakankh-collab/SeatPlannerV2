5246b77de11ff8504049dac760bd99ac318df637
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 03:59:27 +0700

    Update person.js

--- js/person.js
+++ js/person.js
@@ -1,1 +1,49 @@
+let people=[];
 
+async function loadPeople(){
+
+    const res=await fetch("data/person.json");
+
+    people=await res.json();
+
+    renderPeople();
+
+}
+
+function renderPeople(){
+
+    people.forEach(person=>{
+
+        const seat=document.querySelector(
+
+            `[data-id="${person.seat}"]`
+
+        );
+
+        if(!seat) return;
+
+        switch(person.type){
+
+            case "president":
+
+                seat.setAttribute("fill","#F44336");
+
+            break;
+
+            case "vip":
+
+                seat.setAttribute("fill","#FFC107");
+
+            break;
+
+            default:
+
+                seat.setAttribute("fill","#4CAF50");
+
+        }
+
+    });
+
+}
+
+loadPeople();
