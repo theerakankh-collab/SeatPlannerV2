0599fb69562b8a4e38c07a03bebe131d8d410c3e
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 03:24:27 +0700

    Add Engine class with room and people management

--- core/engine.js
+++ core/engine.js
@@ -1,1 +1,23 @@
+export default class Engine{
 
+    constructor(){
+
+        this.room=null;
+
+        this.people=[];
+
+    }
+
+    setRoom(room){
+
+        this.room=room;
+
+    }
+
+    addPeople(list){
+
+        this.people=list;
+
+    }
+
+}
