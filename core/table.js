68e17e932b18d5911c83a116f69b9863fff69f39
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 03:21:49 +0700

    Create Table class with constructor and addChair method

--- core/table.js
+++ core/table.js
@@ -1,1 +1,19 @@
+export default class Table{
 
+    constructor(id,name){
+
+        this.id=id;
+
+        this.name=name;
+
+        this.chairs=[];
+
+    }
+
+    addChair(chair){
+
+        this.chairs.push(chair);
+
+    }
+
+}
