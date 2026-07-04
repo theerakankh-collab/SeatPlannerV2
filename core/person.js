8fe8280e3e5e5c011c0d961fef2125ab0f74b408
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 03:19:35 +0700

    Add Person class to manage employee data

--- core/person.js
+++ core/person.js
@@ -1,1 +1,19 @@
+export default class Person{
 
+    constructor(data){
+
+        this.id=data.id;
+
+        this.name=data.name;
+
+        this.position=data.position;
+
+        this.department=data.department;
+
+        this.type=data.type;
+
+        this.phone=data.phone;
+
+    }
+
+}
