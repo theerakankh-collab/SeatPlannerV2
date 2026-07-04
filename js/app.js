1a9a0ae13450597d15431b9eae2b2927ac3078e1
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-04 22:33:45 +0700

    Implement Excel file import and count display
    Added functionality to import Excel files and display total number of entries.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
+const excelFile =
+document.getElementById("excelFile");
 
+const importBtn =
+document.getElementById("importBtn");
+
+const totalPeople =
+document.getElementById("totalPeople");
+
+let people=[];
+
+importBtn.onclick=function(){
+
+if(excelFile.files.length==0){
+
+alert("เลือกไฟล์ Excel");
+
+return;
+
+}
+
+const reader=
+new FileReader();
+
+reader.onload=function(e){
+
+const data=
+new Uint8Array(e.target.result);
+
+const workbook=
+XLSX.read(data,{type:"array"});
+
+const sheet=
+workbook.Sheets[
+workbook.SheetNames[0]
+];
+
+people=
+XLSX.utils.sheet_to_json(sheet);
+
+totalPeople.innerHTML=
+people.length+" คน";
+
+alert("Import สำเร็จ");
+
+}
+
+reader.readAsArrayBuffer(
+excelFile.files[0]
+);
+
+}
