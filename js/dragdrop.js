0c5975bb96011cea03c98348d67d31c04a7efba1
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 04:16:16 +0700

    Update dragdrop.js

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,1 +1,29 @@
+let dragSeat = null;
 
+document.addEventListener("dragstart", (e) => {
+
+    if (!e.target.classList.contains("seat")) return;
+
+    dragSeat = e.target.dataset.id;
+
+});
+
+document.addEventListener("dragover", (e) => {
+
+    if (!e.target.classList.contains("seat")) return;
+
+    e.preventDefault();
+
+});
+
+document.addEventListener("drop", (e) => {
+
+    if (!e.target.classList.contains("seat")) return;
+
+    e.preventDefault();
+
+    const targetSeat = e.target.dataset.id;
+
+    swapSeat(dragSeat, targetSeat);
+
+});
