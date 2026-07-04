6c24fb7b28dda1c93eb6d85fa38d348d1c56c286
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-05 00:42:01 +0700

    Add seat click event to display seat information

--- js/seat.js
+++ js/seat.js
@@ -1,1 +1,11 @@
+document.addEventListener("click",function(e){
 
+if(e.target.classList.contains("seat")){
+
+document.getElementById("seatInfo").innerHTML=
+
+"รหัสที่นั่ง : "+e.target.dataset.id;
+
+}
+
+});
